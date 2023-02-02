bc01974c8c8c0ac39a432fdba06b78649b26bb03
polaris1009 <gerrad1009@daum.net>
2023-02-02 16:58:55 +0900

    io 부분 부분구현

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,337 @@ use futures::{
 };
 use nix::{
     errno::Errno,
-    sys::{
-        event::{EventFilter, ev_set, EventFlag, kevent, kqueue, KEvent}
-    },
-    unistd::write,
+    sys::event::{kevent, kqueue, EventFilter, EventFlag, FilterFlag, KEvent},
+    unistd::{close, write},
 };
 use std::{
     collections::{HashMap, VecDeque},
     future::Future,
     io::{BufRead, BufReader, BufWriter, Write},
     net::{SocketAddr, TcpListener, TcpStream},
-    os::unix::io::{AsRawFd, RawFd},
+    os::{unix::io::{AsRawFd, RawFd}, fd::AsFd},
     pin::Pin,
     sync::{
         mpsc::{sync_channel, Receiver, SyncSender},
         Arc, Mutex,
     },
-    task::{Context, Poll, Waker}
+    task::{Context, Poll, Waker},
 };
+
+struct Task {
+    future: Mutex<BoxFuture<'static, ()>>,
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let self0 = arc_self.clone();
+        arc_self.sender.send(self0).unwrap();
+    }
+}
+
+struct Executor {
+    sender: SyncSender<Arc<Task>>,
+    receiver: Receiver<Arc<Task>>,
+}
+
+impl Executor {
+    fn new() -> Self {
+        let (sender, receiver) = sync_channel(1024);
+        Executor { sender: sender.clone(), receiver, }
+    }
+
+    fn get_spawner(&self) -> Spawner {
+        Spawner {
+            sender: self.sender.clone(),
+        }
+    }
+
+    fn run(&self) {
+        while let Ok(task) = self.receiver.recv() {
+            let mut future = task.future.lock().unwrap();
+            let waker = waker_ref(&task);
+            let mut ctx = Context::from_waker(&waker);
+            let _ = future.as_mut().poll(&mut ctx);
+        }
+    }
+}
+
+struct Spawner {
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(future),
+            sender: self.sender.clone(),
+        });
+
+        self.sender.send(task).unwrap();
+    }
+}
+
+
+struct Hello {
+    state: StateHello,
+}
+
+enum StateHello {
+    HELLO,
+    WORLD,
+    END,
+}
+
+impl Hello {
+    fn new() -> Self {
+        Hello {
+            state: StateHello::HELLO,
+        }
+    }
+}
+
+impl Future for Hello {
+    type Output = ();
+    
+    fn poll(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        match (*self).state {
+            StateHello::HELLO => {
+                print!("Hello, ");
+                (*self).state = StateHello::WORLD;
+                cx.waker().wake_by_ref();
+                return Poll::Pending;
+            }
+            StateHello::WORLD => {
+                println!("World!");
+                (*self).state = StateHello::END;
+                cx.waker().wake_by_ref();
+                return Poll::Pending;
+            }
+            StateHello::END => {
+                return Poll::Ready(());
+            }
+        }
+    }
+}
+
 
 fn write_eventfd(fd: RawFd, n: usize) {
     let ptr = &n as *const usize as *const u8;
-    let val = unsafe {
-        std::slice::from_raw_parts(ptr, std::mem::size_of_val(&n))
-    };
+    let val = unsafe { std::slice::from_raw_parts(ptr, std::mem::size_of_val(&n)) };
     write(fd, &val).unwrap();
 }
 
+enum IOOps {
+    ADD(EventFilter, RawFd, Waker),
+    REMOVE(RawFd),
+}
+
+struct IOSelector {
+    wakers: Mutex<HashMap<RawFd, Waker>>, // fd waker
+    queue: Mutex<VecDeque<IOOps>>,        // IO 큐
+    kqfd: RawFd,                          // kqueue의 fd
+    event: RawFd,                         // eventfd 의 fd
+}
+
+impl IOSelector {
+    fn new(event: RawFd) -> Arc<Self> {
+        let s = IOSelector {
+            wakers: Mutex::new(HashMap::new()),
+            queue: Mutex::new(VecDeque::new()),
+            kqfd: kqueue().unwrap(),
+            event,
+        };
+        let result = Arc::new(s);
+        let s = result.clone();
+
+        std::thread::spawn(move || s.select());
+
+        result
+    }
+
+    fn add_event(&self, flag: EventFlag, waker: Waker, wakers: &mut HashMap<RawFd, Waker>) {
+        let kqueue_add = EventFlag::EV_ADD;
+        let kqueue_one = EventFlag::EV_ONESHOT;
+        let mut ev = KEvent::new(
+            self.event as usize,
+            EventFilter::EVFILT_READ,
+            kqueue_one | flag,
+            FilterFlag::all(),
+            0,
+            0,
+        );
+
+        assert!(!wakers.contains_key(&self.event));
+        wakers.insert(self.event, waker);
+    }
+
+    fn rm_event(&self, fd: RawFd, wakers: &mut HashMap<RawFd, Waker>) {
+        close(fd).unwrap();
+        wakers.remove(&fd);
+    }
+
+    fn select(&self) {
+        let kqueue_read = EventFilter::EVFILT_READ;
+        let kqueue_add = EventFlag::EV_ADD;
+
+        let mut events: Vec<KEvent> = vec![];
+        let mut changes: Vec<KEvent> = vec![];
+
+        while let Ok(n) = kevent(self.kqfd, &changes, &mut events, 0) {
+            let mut t = self.wakers.lock().unwrap();
+            for n in 0..n {
+                if events[n].data() == self.event as isize {
+                    let mut q = self.queue.lock().unwrap();
+                    while let Some(op) = q.pop_front() {
+                        match op {
+                            IOOps::ADD(flag, fd, waker) => self.add_event(EventFlag::EV_ADD, waker, &mut t),
+                            IOOps::REMOVE(fd) => self.rm_event(fd, &mut t),
+                        }
+                    }
+                } else {
+                    let data = events[n].data() as i32;
+                    let waker = t.remove(&data).unwrap();
+                    waker.wake_by_ref();
+                }
+            }
+        }
+    }
+
+    fn register(&self, flags: EventFilter, waker: Waker) {
+        let mut q = self.queue.lock().unwrap();
+        q.push_back(IOOps::ADD(flags, self.event, waker));
+        write_eventfd(self.event, 1);
+    }
+
+    fn unregister(&self) {
+        let mut q = self.queue.lock().unwrap();
+        q.push_back(IOOps::REMOVE(self.event));
+        write_eventfd(self.event, 1);
+    }
+}
+
+struct AsyncListener {
+    listener: TcpListener,
+    selector: Arc<IOSelector>,
+}
+
+impl AsyncListener {
+    fn listen(addr: &str, selector: Arc<IOSelector>) -> AsyncListener {
+        let listener = TcpListener::bind(addr).unwrap();
+
+        listener.set_nonblocking(true).unwrap();
+
+        AsyncListener {
+            listener: listener,
+            selector: selector,
+        }
+    }
+
+    fn accept(&self) -> Accept {
+        Accept { listener: self }
+    }
+}
+
+impl Drop for AsyncListener {
+    fn drop(&mut self) {
+        self.selector.unregister();
+    }
+}
+
+struct Accept<'a> {
+    listener: &'a AsyncListener,
+}
+
+impl<'a> Future for Accept<'a> {
+    type Output = (AsyncReader, BufWriter<TcpStream>, SocketAddr);
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.listener.listener.accept() {
+            Ok((stream, addr)) => {
+                let stream0 = stream.try_clone().unwrap();
+                Poll::Ready((
+                    AsyncReader::new(stream0, self.listener.selector.clone()),
+                    BufWriter::new(stream),
+                    addr,
+                ))
+            }
+            Err(err) => {
+                if err.kind() == std::io::ErrorKind::WouldBlock {
+                    self.listener.selector.register(
+                        EventFilter::EVFILT_READ,
+                        // self.listener.listener.as_raw_fd(),
+                        cx.waker().clone(),
+                    );
+                    Poll::Pending
+                } else {
+                    panic!("accept: {}", err);
+                }
+            }
+        }
+    }
+}
+
+struct AsyncReader {
+    fd: RawFd,
+    reader: BufReader<TcpStream>,
+    selector: Arc<IOSelector>,
+}
+
+impl AsyncReader {
+    fn new(stream: TcpStream, selector: Arc<IOSelector>) -> AsyncReader {
+        stream.set_nonblocking(true).unwrap();
+        AsyncReader {
+            fd: stream.as_raw_fd(),
+            reader: BufReader::new(stream),
+            selector: selector,
+        }
+    }
+
+    fn read_line(&mut self) -> ReadLine {
+        ReadLine { reader: self }
+    }
+}
+
+impl Drop for AsyncReader {
+    fn drop(&mut self) {
+        self.selector.unregister();
+    }
+}
+
+struct ReadLine<'a> {
+    reader: &'a mut AsyncReader,
+}
+
+impl<'a> Future for ReadLine<'a> {
+    type Output = Option<String>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut line = String::new();
+        match self.reader.reader.read_line(&mut line) {
+            Ok(0) => Poll::Ready(None),
+            Ok(_) => Poll::Ready(Some(line)),
+            Err(err) => {
+                if err.kind() == std::io::ErrorKind::WouldBlock {
+                    self.reader
+                        .selector
+                        .register(EventFilter::EVFILT_READ, cx.waker().clone());
+                    Poll::Pending
+                } else {
+                    Poll::Ready(None)
+                }
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let executor = Executor::new();
+    let listener = TcpListener::bind("127.0.0.1:10000").unwrap();
+    // println!("{:?}", );
+    let fd = listener.as_fd().as_raw_fd().clone();
+
+    let selector = IOSelector::new(fd);
+    let spawner = executor.get_spawner();
 }
